a1ca5e19aabd4dc7df8e3ebe658d342dcc0a5e17
test <testcf77@gmail.com>
2017-09-13 16:46:51 -0500

    ud120 udacity machine learning course solutions

---
+++ naive_bayes/nb_author_id.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+
+    Use a Naive Bayes Classifier to identify emails by their authors
+    
+    authors and labels:
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, , labels_train, labels_test = preprocess()
+
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+print(clf.predict(features_test))
+
+#########################################################
+### your code goes here ###
+
+
+#########################################################
+
+
---
+++ outliers/outlier_cleaner.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+
+def outlierCleaner(predictions, ages, net_worths):
+    """
+        Clean away the 10% of points that have the largest
+        residual errors (difference between the prediction
+        and the actual net worth).
+
+        Return a list of tuples named cleaned_data where 
+        each tuple is of the form (age, net_worth, error).
+    """
+    
+    cleaned_data = []
+
+    ### your code goes here
+    
+    #print(ages)
+    
+    zipped = zip(ages, net_worths,abs(predictions-net_worths))
+    cleaned_data = list(zipped)
+    
+    cleaned_data.sort(key=lambda tup: tup[2])
+    
+    return cleaned_data[:round(len(cleaned_data)*0.9)]
+
